e7947626d64ae171f56f6f6a8b6dc339f15368eb
Liliana Loaiza <liliana.loaiza@globant.com>
2023-10-08 20:43:13 -0500

    [SQL] Window Function: exam with chatcito, 5 Qs

--- SQL/window_functions.sql
+++ SQL/window_functions.sql
@@ -161,3 +161,179 @@ SELECT
 FROM races
 WHERE finish
 ORDER BY year desc, circuit_name, time_at;
+
+
+-- ------------------------------------------------------------------------
+-- *****  Exam with chatcito  *****
+-- ------------------------------------------------------------------------
+
+-- 1) Find the cumulative sum of order_amount for each customer_id over time,
+-- ordered by order_date.
+CREATE TABLE sales (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    order_amount DECIMAL(10, 2)
+);
+
+INSERT INTO sales (order_id, customer_id, order_date, order_amount)
+VALUES
+    (1, 101, '2023-01-01', 100.50),
+    (2, 102, '2023-01-02', 75.25),
+    (3, 101, '2023-01-03', 50.75),
+    (4, 103, '2023-01-04', 200.00),
+    (5, 102, '2023-01-05', 125.00),
+    (6, 101, '2023-01-06', 75.50),
+    (7, 103, '2023-01-07', 150.25);
+
+-- Query_e1
+SELECT
+    customer_id,
+    order_date,
+    order_amount,
+    sum(order_amount) OVER (PARTITION BY customer_id ORDER BY order_date) AS cumulative
+FROM sales
+ORDER BY
+    customer_id,
+    order_date;
+    
+
+-- 2) Find the highest salary in each department and the employee(s) who earn that salary.
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    department_id INT,
+    salary DECIMAL(10, 2),
+    hire_date DATE
+);
+
+INSERT INTO employees (employee_id, department_id, salary, hire_date)
+VALUES
+    (1, 101, 55000.00, '2022-01-01'),
+    (2, 102, 60000.00, '2021-03-15'),
+    (3, 101, 60000.00, '2022-02-15'),
+    (4, 103, 75000.00, '2021-05-20'),
+    (5, 102, 65000.00, '2021-08-10'),
+    (6, 103, 80000.00, '2022-04-05'),
+    (7, 101, 58000.00, '2023-01-10'),
+    (8, 101, 60000.00, '2022-04-20');
+
+-- Query_e2
+SELECT
+    department_id, employee_id, salary AS highest_salary
+FROM (
+    SELECT
+        department_id,
+        employee_id,
+        salary,
+        rank() OVER (PARTITION BY department_id ORDER BY salary DESC) = 1 AS has_highest
+    FROM employees
+) AS tbl
+WHERE has_highest;
+
+
+-- 3) Find the customer(s) with the highest total orders.
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    order_total DECIMAL(10, 2)
+);
+
+INSERT INTO orders (order_id, customer_id, order_date, order_total)
+VALUES
+    (1, 101, '2023-01-01', 500.00),
+    (2, 102, '2023-01-02', 750.00),
+    (3, 101, '2023-01-03', 600.00),
+    (4, 103, '2023-01-04', 800.00),
+    (5, 102, '2023-01-05', 900.00),
+    (6, 101, '2023-01-06', 700.00),
+    (7, 103, '2023-01-07', 950.00),
+    (8, 102, '2023-01-08', 150.00);
+
+-- Query_e3
+SELECT
+    customer_id, sum_order_total
+FROM (
+    SELECT
+        customer_id,
+        sum(order_total) AS sum_order_total,
+        dense_rank() OVER (ORDER BY sum(order_total) DESC) AS rank
+    FROM orders
+    GROUP BY customer_id
+) AS tbl
+WHERE rank = 1;
+
+
+-- 4) Each student can take the same course multiple times. Find the latest grade
+-- for each student in each course.
+DROP TABLE IF EXISTS students;
+CREATE TABLE students (
+    student_id INT,
+    course_id INT,
+    grade CHAR(1),
+    exam_date DATE
+);
+
+INSERT INTO students (student_id, course_id, grade, exam_date)
+VALUES
+    (1, 101, 'A', '2023-01-15'),
+    (1, 101, 'B', '2023-02-10'),
+    (2, 102, 'C', '2023-03-05'),
+    (2, 102, 'B', '2023-04-20'),
+    (3, 101, 'A', '2023-02-05'),
+    (3, 101, 'A', '2023-03-15'),
+    (3, 102, 'B', '2023-04-10');
+
+-- Query_e4
+SELECT
+    student_id, course_id, grade AS latest_grade
+FROM (
+    SELECT
+        student_id,
+        course_id,
+        grade,
+        dense_rank() OVER (PARTITION BY student_id, course_id ORDER BY exam_date DESC) AS rank
+    FROM students
+    ORDER BY student_id, course_id, exam_date DESC
+) AS tbl
+WHERE rank = 1;
+
+
+-- 5) Find the top-selling product for each month of the year,
+-- along with the total quantity sold and revenue for that product in each month.
+DROP TABLE IF EXISTS sales_transactions;
+CREATE TABLE sales_transactions (
+    transaction_id INT PRIMARY KEY,
+    product_id INT,
+    transaction_date DATE,
+    quantity_sold INT,
+    revenue DECIMAL(10, 2)
+);
+
+INSERT INTO sales_transactions (transaction_id, product_id, transaction_date, quantity_sold, revenue)
+VALUES
+    (1, 101, '2023-01-15', 50, 1000.00),
+    (2, 102, '2023-01-20', 30, 750.00),
+    (3, 101, '2023-02-10', 45, 900.00),
+    (4, 102, '2023-02-15', 40, 800.00),
+    (5, 103, '2023-03-05', 60, 1200.00),
+    (6, 101, '2023-03-15', 55, 1100.00),
+    (7, 103, '2023-03-20', 65, 1300.00),
+    (8, 102, '2023-04-10', 35, 700.00),
+    (9, 101, '2023-04-20', 25, 500.00);
+
+-- Query_e5
+SELECT DISTINCT
+    to_char(transaction_date, 'yyyy-MM') AS month,
+    first_value(product_id) OVER (
+        PARTITION BY to_char(transaction_date, 'yyyy-MM') 
+        ORDER BY revenue DESC) AS selling_product_per_month,
+    product_id,
+    sum(quantity_sold) OVER (
+        PARTITION BY product_id, to_char(transaction_date, 'yyyy-MM')) AS quantity_per_product,
+    sum(revenue) OVER (
+        PARTITION BY product_id, to_char(transaction_date, 'yyyy-MM')) AS revenue_per_product
+FROM sales_transactions
+ORDER BY month, product_id;
