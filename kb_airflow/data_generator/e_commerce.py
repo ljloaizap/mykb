765f63f2691af9c2b97d72ca899871540e1d027d
Liliana Loaiza <liliana.loaiza@globant.com>
2023-10-17 20:28:17 -0500

    [Airflow] Add data generator e-commerce product

---
+++ kb_airflow/data_generator/e_commerce.py
@@ -0,0 +1,108 @@
+import random
+import time
+from datetime import datetime
+from faker import Faker
+import psycopg2
+from psycopg2.extras import execute_values
+import sys
+
+
+def get_product_categories():
+    """PH"""
+
+    return [
+        "Electronics",
+        "Clothing",
+        "Home and Garden",
+        "Health and Beauty",
+        "Automotive",
+        "Sports and Outdoors",
+        "Toys and Games",
+        "Jewelry",
+        "Books and Literature",
+        "Food and Groceries",
+        "Furniture",
+        "Pet Supplies",
+        "Office Supplies",
+        "Music and Instruments",
+        "Baby and Toddler",
+        "Art and Craft Supplies",
+        "Shoes and Footwear",
+        "Watches",
+        "Tools and Hardware",
+        "Kitchen and Dining",
+        "Travel and Luggage",
+        "Fitness and Exercise",
+        "Home Improvement",
+        "Computers and Accessories",
+        "Party and Event Supplies",
+        "Gardening and Outdoor Decor",
+        "Beauty and Personal Care",
+        "Cameras and Photography",
+        "School and Educational Supplies",
+        "Home Appliances"
+    ]
+
+
+def generate_fake_report():
+    '''PH'''
+
+    # Establish a connection to your PostgreSQL database
+    conn = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="postgres",
+        host="localhost"
+    )
+    cursor = conn.cursor()
+    categories = get_product_categories()
+
+    # Create a Faker instance to generate dummy data
+    fake = Faker()
+
+    while True:
+        try:
+            # Generate a single record
+            record = (
+                fake.word(),                    # Product Name
+                fake.sentence(),                # Description
+                random.choice(categories),      # Category
+                random.uniform(10, 1000),       # Price
+                random.randint(0, 100),         # Stock Quantity
+                fake.word(),                    # SKU
+                fake.company(),                 # Brand
+                fake.image_url(),               # Image URL
+                random.uniform(0.1, 50),        # Weight
+                random.uniform(1, 100),         # Length
+                random.uniform(1, 100),         # Width
+                random.uniform(1, 100),         # Height
+                round(random.uniform(1, 5), 2),  # Product Rating
+            )
+
+            # Insert the record into the "products" table
+            cursor = conn.cursor()
+            insert_query = '''
+                INSERT INTO e_commerce.product
+                    (product_name, description, category, price, stock_quantity, sku, brand, image_url, weight, length, width, height, product_rating)
+                VALUES %s
+            '''
+            execute_values(cursor, insert_query, [record])
+            conn.commit()
+            cursor.close()
+            print(
+                f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S:%f")}] Record created...')
+
+            # Wait for a random time between 1 and 4 seconds before creating the next record
+            sleep_time = random.uniform(0, 0.1)
+            time.sleep(sleep_time)
+        except KeyboardInterrupt:
+            # Stop the program when interrupted (e.g., using Ctrl+C)
+            conn.close()
+            sys.exit(1)
+        except Exception as e:
+            print(f'There was an error: {type(e)}')
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    generate_fake_report()
