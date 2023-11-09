ed04f28a30aead5956a9ff116a47543ebaa61eb8
Liliana Loaiza <liliana.loaiza@globant.com>
2023-11-09 18:13:24 -0500

    :memo: [Airflow] Update extract in ETL

--- kb_airflow/dags/etl_workflow.py
+++ kb_airflow/dags/etl_workflow.py
@@ -1,52 +1,53 @@
 import os
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+from airflow.operators.python import PythonOperator, get_current_context
 from airflow.operators.dummy import DummyOperator
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from pendulum.datetime import DateTime
+from pendulum import from_format, now
 from sqlalchemy import create_engine
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 10, 18)
+    'start_date': datetime(2023, 11, 1)
 }
-
-
-def get_csv_list_per_interval(ini_date: datetime, end_date: datetime):
-    '''PH'''
-
-    return ['product_20231026-170000.csv']
 
 
 with DAG(
     dag_id=os.path.basename(__file__).replace(".py", ""),
-    description='ETL to process e-commerce product data from .CSV files',
+    description='ETL to process e-commerce product data',
     default_args=default_args,
-    schedule_interval='0 5 * * *'
+    schedule_interval='*/20 * * * *'
 ) as dag:
 
     ###################### Functions ######################
-    def connect_to_db_with_sql_alchemy(conn_str):
-        '''PH'''
-        conn_str = 'postgresql://postgres:postgres@172.20.0.1:5432/postgres'
-        engine = create_engine(conn_str)
 
-        try:
-            with engine.connect() as connection:
-                print('>> Connection was successful!!!!')
-                result = connection.execute(
-                    "SELECT count(*) FROM e_commerce.product;")
-                row_count = result.scalar()
-                print(f">> Row count in 'product' table: {row_count}")
-        except Exception as ex:
-            print(f'>> Errorcillo: {ex} <<<')
-
-    def extracts_csv_data():
+    def extracts_data():
         '''PH'''
         load_dotenv()
         conn_str = os.getenv('DB_CONN_STR')
-        print(f'Extracting .CSV data... {conn_str} -- v10')
-        connect_to_db_with_sql_alchemy(conn_str)
+
+        try:
+            engine = create_engine(conn_str)
+            context = get_current_context()
+            str_format = '%Y-%m-%d %H:%M:%S'
+            start_date = context['data_interval_start'].strftime(str_format)
+            end_date = context['data_interval_end'].strftime(str_format)
+            print(f'>> start={start_date}; end={end_date}')
+
+            with engine.connect() as connection:
+                result = connection.execute(f"""
+                    SELECT count(1)
+                    FROM e_commerce.product AS p
+                    WHERE p.created_at BETWEEN '{start_date}' AND '{end_date}'
+                """)
+                row_count = result.scalar()
+                print(
+                    f">> Row count in 'e_commerce.product' table: {row_count}")
+        except Exception as ex:
+            print(f'>> Errorcillo: {ex} <<<')
 
     def transform_data():
         '''PH'''
@@ -59,7 +60,7 @@ with DAG(
     ###################### Tasks ######################
     task_extract = PythonOperator(
         task_id='extract_data',
-        python_callable=extracts_csv_data
+        python_callable=extracts_data
     )
 
     task_transform = PythonOperator(
